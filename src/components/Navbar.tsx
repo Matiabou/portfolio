d3dba443af37784d101473206e8cf601819fd0bf
unknown <matiasaboudara@gmail.com>
2025-07-08 17:19:15 -0300

    Navbar y estilo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import '../styles/Navbar.css'
+
+export function Navbar() {
+    return (
+        <header>
+            <nav>
+            <a>
+                Inicio
+            </a>
+            <a>
+                Proyectos
+            </a>
+            <a>
+                Sobre mí
+            </a>
+            <a>
+                Contacto
+            </a>
+            </nav>
+        </header>
+    )
+}
